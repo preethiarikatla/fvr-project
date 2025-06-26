ce9f231e4e0f00a811c0814a2474ce7217a8fcf5
pree <123841299+preethiarikatla@users.noreply.github.com>
2025-06-26 19:24:09 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,13 +16,6 @@ resource "azurerm_resource_group" "rg" {
   location = "East US"
 }
 
-resource "azurerm_virtual_network" "vnet" {
-  name                = "vnet-test"
-  address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-}
-
 resource "azurerm_subnet" "subnet1" {
   name                 = "subnet-1"
   resource_group_name  = azurerm_resource_group.rg.name
