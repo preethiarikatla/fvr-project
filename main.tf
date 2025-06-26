a139036442e96c4288e7e665ee4e6c8d68ba3dbf
pree <123841299+preethiarikatla@users.noreply.github.com>
2025-06-26 19:25:39 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,16 +6,25 @@ terraform {
     }
   }
 }
-
 provider "azurerm" {
-  features {}  # This is required
+  features {}
 }
 
+# Resource Group
 resource "azurerm_resource_group" "rg" {
   name     = "rg-recreate-test"
   location = "East US"
 }
 
+# Virtual Network
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnet-test"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+# Subnet 1
 resource "azurerm_subnet" "subnet1" {
   name                 = "subnet-1"
   resource_group_name  = azurerm_resource_group.rg.name
@@ -23,6 +32,7 @@ resource "azurerm_subnet" "subnet1" {
   address_prefixes     = ["10.0.1.0/24"]
 }
 
+# Subnet 2
 resource "azurerm_subnet" "subnet2" {
   name                 = "subnet-2"
   resource_group_name  = azurerm_resource_group.rg.name
@@ -30,6 +40,7 @@ resource "azurerm_subnet" "subnet2" {
   address_prefixes     = ["10.0.2.0/24"]
 }
 
+# Network Interface (initially attached to subnet1)
 resource "azurerm_network_interface" "nic" {
   name                = "nic-recreate-test"
   location            = azurerm_resource_group.rg.location
@@ -40,13 +51,5 @@ resource "azurerm_network_interface" "nic" {
     subnet_id                     = azurerm_subnet.subnet1.id
     private_ip_address_allocation = "Dynamic"
   }
-}
-
-# Subnet
-resource "azurerm_subnet" "subnet" {
-  name                 = "gopi-subnet"
-  resource_group_name  = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.0.1.0/24"]
 }
 
