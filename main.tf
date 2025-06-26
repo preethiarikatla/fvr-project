5a01d55b6a093eb14ee8e92923f7d9ae5432c855
pree <123841299+preethiarikatla@users.noreply.github.com>
2025-06-26 19:20:27 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,22 +11,42 @@ provider "azurerm" {
   features {}  # This is required
 }
 
-# Resource Group
 resource "azurerm_resource_group" "rg" {
-  name     = "gopi-rg"
+  name     = "rg-recreate-test"
   location = "East US"
-  tags = {
-    environment = "preethii"
-    owner       = "gopi"
-  }
 }
 
-# Virtual Network
 resource "azurerm_virtual_network" "vnet" {
-  name                = "gopi-vnet"
+  name                = "vnet-test"
   address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_subnet" "subnet1" {
+  name                 = "subnet-1"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_subnet" "subnet2" {
+  name                 = "subnet-2"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_network_interface" "nic" {
+  name                = "nic-recreate-test"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet1.id
+    private_ip_address_allocation = "Dynamic"
+  }
 }
 
 # Subnet
