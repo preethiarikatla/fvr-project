2d993d5e084f2a6e4521873caa79f7ce16090a2f
pree <123841299+preethiarikatla@users.noreply.github.com>
2025-06-26 19:39:55 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,46 +10,14 @@ provider "azurerm" {
   features {}
 }
 
-# Resource Group
 resource "azurerm_resource_group" "rg" {
-  name     = "rg-recreate-test"
+  name     = "rg-pip-test"
   location = "East US"
 }
 
-# Virtual Network
-resource "azurerm_virtual_network" "vnet" {
-  name                = "vnet-test"
-  address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.rg.location
+resource "azurerm_public_ip" "pip" {
+  name                = "pip-recreate-test"
   resource_group_name = azurerm_resource_group.rg.name
-}
-
-# Subnet 1
-resource "azurerm_subnet" "subnet1" {
-  name                 = "subnet-1"
-  resource_group_name  = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.0.1.0/24"]
-}
-
-# Subnet 2
-resource "azurerm_subnet" "subnet2" {
-  name                 = "subnet-2"
-  resource_group_name  = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.0.2.0/24"]
-}
-
-# Network Interface (initially attached to subnet1)
-resource "azurerm_network_interface" "nic" {
-  name                = "nic-recreate-test"
   location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-
-  ip_configuration {
-    name                          = "internal"
-    subnet_id                     = azurerm_subnet.subnet1.id
-    private_ip_address_allocation = "Dynamic"
-  }
+  public_ip_address_allocation = "Static"
 }
-
