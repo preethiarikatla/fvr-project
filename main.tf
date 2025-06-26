a4f8bff20b5327d327115ab5d7807a7096561fb5
pree <123841299+preethiarikatla@users.noreply.github.com>
2025-06-26 19:57:09 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -19,18 +19,26 @@ resource "azurerm_resource_group" "rg" {
 }
 
 resource "azurerm_cosmosdb_account" "cosmos" {
-  name                = "cosmos-recreate-test"
+  name                = "cosmosrecreatetestacct"  # must be globally unique
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   offer_type          = "Standard"
   kind                = "GlobalDocumentDB"
+
   consistency_policy {
     consistency_level = "Session"
   }
+
   geo_location {
     location          = azurerm_resource_group.rg.location
     failover_priority = 0
   }
-  throughput = 400  # This is ForceNew
+}
+
+resource "azurerm_cosmosdb_sql_database" "db" {
+  name                = "testdb"
+  resource_group_name = azurerm_resource_group.rg.name
+  account_name        = azurerm_cosmosdb_account.cosmos.name
+  throughput          = 400  # 🔥 ForceNew field
 }
 
