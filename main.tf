244a47d203fde2c88ebc093aae0e7f2b5df8e591
pree <123841299+preethiarikatla@users.noreply.github.com>
2025-06-26 23:24:46 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -41,14 +41,14 @@ resource "azurerm_virtual_network_peering" "vnet1_to_vnet2" {
 }
 
 # Peering from vnet-destination to vnet-source
-resource "azurerm_virtual_network_peering" "vnet2_to_vnet1" {
-  name                      = "peer-vnet2-to-vnet1-gopi"  # Different name from above
-  resource_group_name       = azurerm_resource_group.example.name
-  virtual_network_name      = azurerm_virtual_network.vnet2.name
-  remote_virtual_network_id = azurerm_virtual_network.vnet1.id
-  allow_virtual_network_access = true
-  allow_forwarded_traffic      = true
-}
+#resource "azurerm_virtual_network_peering" "vnet2_to_vnet1" {
+#  name                      = "peer-vnet2-to-vnet1-gopi"  # Different name from above
+#  resource_group_name       = azurerm_resource_group.example.name
+#  virtual_network_name      = azurerm_virtual_network.vnet2.name
+#  remote_virtual_network_id = azurerm_virtual_network.vnet1.id
+#  allow_virtual_network_access = true
+#  allow_forwarded_traffic      = true
+#}
 
 
 
