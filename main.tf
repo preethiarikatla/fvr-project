add210769e4b34b1a7bed05c3b42566abad1d727
pree <123841299+preethiarikatla@users.noreply.github.com>
2025-06-26 22:33:06 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -41,7 +41,7 @@ resource "azurerm_virtual_network_peering" "vnet1_to_vnet2" {
 
 # Peering from vnet-destination to vnet-source
 resource "azurerm_virtual_network_peering" "vnet2_to_vnet1" {
-  name                      = "peer-vnet2-to-vnet1"
+  name                      = "peer-vnet2-to-vnet1-manual"
   resource_group_name       = azurerm_resource_group.example.name
   virtual_network_name      = azurerm_virtual_network.vnet2.name
   remote_virtual_network_id = azurerm_virtual_network.vnet1.id
