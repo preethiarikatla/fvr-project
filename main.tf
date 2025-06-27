ec2c1bf4528a909122b3c682d93135dcb9db30fc
pree <123841299+preethiarikatla@users.noreply.github.com>
2025-06-26 22:15:24 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -29,13 +29,25 @@ resource "azurerm_virtual_network" "vnet2" {
   resource_group_name = azurerm_resource_group.example.name
 }
 
-resource "azurerm_virtual_network_peering" "peer" {
-  name                      = "peer-vnet2-to-vnet2"
+# Peering from vnet-source to vnet-destination
+resource "azurerm_virtual_network_peering" "vnet1_to_vnet2" {
+  name                      = "peer-vnet1-to-vnet2"
   resource_group_name       = azurerm_resource_group.example.name
   virtual_network_name      = azurerm_virtual_network.vnet1.name
   remote_virtual_network_id = azurerm_virtual_network.vnet2.id
-  allow_forwarded_traffic   = true
   allow_virtual_network_access = true
+  allow_forwarded_traffic      = true
 }
+
+# Peering from vnet-destination to vnet-source
+resource "azurerm_virtual_network_peering" "vnet2_to_vnet1" {
+  name                      = "peer-vnet2-to-vnet1"
+  resource_group_name       = azurerm_resource_group.example.name
+  virtual_network_name      = azurerm_virtual_network.vnet2.name
+  remote_virtual_network_id = azurerm_virtual_network.vnet1.id
+  allow_virtual_network_access = true
+  allow_forwarded_traffic      = true
+}
+
 
 
