8d15c5ae2dbc6c07748f081fef907300d61fb930
pree <123841299+preethiarikatla@users.noreply.github.com>
2025-07-02 15:31:41 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~>4.19.0"  # Ensuring compatibility with the installed version
+      version = "~>4.19.0"
     }
   }
 }
@@ -30,25 +30,27 @@ resource "azurerm_virtual_network" "vnet2" {
 }
 
 # Peering from vnet-source to vnet-destination
-# Peering from vnet-source to vnet-destination
 resource "azurerm_virtual_network_peering" "vnet1_to_vnet2" {
-  name                      = "peer-vnet1-to-vnet2-nani"  # Unique name
-  resource_group_name       = azurerm_resource_group.example.name
-  virtual_network_name      = azurerm_virtual_network.vnet1.name
-  remote_virtual_network_id = azurerm_virtual_network.vnet2.id
-  allow_virtual_network_access = true
-  allow_forwarded_traffic      = true
+  name                          = "peer-vnet1-to-vnet2-nani"
+  resource_group_name           = azurerm_resource_group.example.name
+  virtual_network_name          = azurerm_virtual_network.vnet1.name
+  remote_virtual_network_id     = azurerm_virtual_network.vnet2.id
+  allow_virtual_network_access  = true
+  allow_forwarded_traffic       = true
+  allow_gateway_transit         = false
+  use_remote_gateways           = false
 }
 
-# Peering from vnet-destination to vnet-source
-#resource "azurerm_virtual_network_peering" "vnet2_to_vnet1" {
-#  name                      = "peer-vnet2-to-vnet1-gopi"  # Different name from above
-#  resource_group_name       = azurerm_resource_group.example.name
-#  virtual_network_name      = azurerm_virtual_network.vnet2.name
-#  remote_virtual_network_id = azurerm_virtual_network.vnet1.id
-#  allow_virtual_network_access = true
-#  allow_forwarded_traffic      = true
-#}
+# Cognitive Deployment with optional + computed field
+resource "azurerm_cognitive_deployment" "example" {
+  name                = "cognitive-deploy-test"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  cognitive_account_id = azurerm_cognitive_account.example.id
+
+  version_upgrade_option = "OnceNewDefaultVersionAvailable" # Optional + Computed
+}
+
 
 
 
