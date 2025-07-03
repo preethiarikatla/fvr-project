d11b392e742546893069af05f04a8f8fb8030176
pree <123841299+preethiarikatla@users.noreply.github.com>
2025-07-03 14:40:01 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,66 +11,30 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "example" {
-  name     = "rg-ignore-appgw"
+  name     = "rg-ignore-test"
   location = "East US"
 }
 
-resource "azurerm_virtual_network" "example" {
-  name                = "vnet-ignore-appgw"
-  address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-}
-
-resource "azurerm_subnet" "example" {
-  name                 = "subnet-appgw"
-  resource_group_name  = azurerm_resource_group.example.name
-  virtual_network_name = azurerm_virtual_network.example.name
-  address_prefixes     = ["10.0.1.0/24"]
-}
-
-resource "azurerm_public_ip" "example" {
-  name                = "pip-ignore-appgw"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-  allocation_method   = "Static"
-  sku                 = "Standard"
-}
-
-resource "azurerm_application_gateway" "example" {
-  name                = "appgw-ignore-test"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-
-  sku {
-    name     = "Standard_v2"
-    tier     = "Standard_v2"
-    capacity = 1
-  }
-
-  gateway_ip_configuration {
-    name      = "appgw-ip-config"
-    subnet_id = azurerm_subnet.example.id
-  }
-
-  frontend_port {
-    name = "frontend-port"
-    port = 80
-  }
-
-  frontend_ip_configuration {
-    name                 = "appgw-fe-ip"
-    public_ip_address_id = azurerm_public_ip.example.id
-  }
+resource "azurerm_storage_account" "example" {
+  name                     = "ignoreteststorage123"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
 
   tags = {
-    environment = "dev"
+    environment = "test"
+  }
+
+  custom_domain {
+    name          = "www.example.com"
+    use_subdomain = false
   }
 
   lifecycle {
     ignore_changes = [
-      tags,                               # top-level
-      frontend_ip_configuration[0].name   # nested block
+      tags,                  # top-level attribute
+      custom_domain[0].name  # nested block
     ]
   }
 }
