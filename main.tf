2c35c81f3e2f844508a3f12ca4adfb733585cc52
pree <123841299+preethiarikatla@users.noreply.github.com>
2025-07-08 15:34:00 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -39,7 +39,16 @@ resource "azurerm_virtual_network_peering" "vnet1_to_vnet2" {
   allow_gateway_transit         = false
   use_remote_gateways           = false
 }
-
+resource "azurerm_virtual_network_peering" "vnet2_to_vnet1" {
+  name                          = peer-vnet2-to-vnet1
+  resource_group_name           = azurerm_resource_group.example.name
+  virtual_network_name          = azurerm_virtual_network.vnet2.name
+  remote_virtual_network_id     = azurerm_virtual_network.vnet1.id
+  allow_virtual_network_access  = true
+  allow_forwarded_traffic       = true
+  allow_gateway_transit         = false
+  use_remote_gateways           = false
+}
 resource "azurerm_network_security_group" "nsg" {
   name                = var.pree
   location            = azurerm_resource_group.example.location
