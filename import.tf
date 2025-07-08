f823f9727cf945f3b4c2e042e5f288be6edacf9b
pree <123841299+preethiarikatla@users.noreply.github.com>
2025-07-08 18:26:13 -0400

    Update import.tf

--- import.tf
+++ import.tf
@@ -1,9 +1,10 @@
 import {
-  id = "/subscriptions/7e7e9b8c-d389-4d54-ae57-03a451e26d57/resourceGroups/rg-ignore-testt/providers/Microsoft.Network/virtualNetworks/vnet-source"
+  id = "/subscriptions/7e7e9b8c-d389-4d54-ae57-03a451e26d57/resourceGroups/rg-ignore-testt/providers/Microsoft.Network/virtualNetworks/vnet-source/virtualNetworkPeerings/peer-vnet1-to-vnet2-nani"
   to = azurerm_virtual_network_peering.vnet1_to_vnet2
 }
 
 import {
-  id = "/subscriptions/7e7e9b8c-d389-4d54-ae57-03a451e26d57/resourceGroups/rg-ignore-testt/providers/Microsoft.Network/virtualNetworks/vnet-destination"
+  id = "/subscriptions/7e7e9b8c-d389-4d54-ae57-03a451e26d57/resourceGroups/rg-ignore-testt/providers/Microsoft.Network/virtualNetworks/vnet-destination/virtualNetworkPeerings/peer-vnet1-to-vnet2-nani"
   to = azurerm_virtual_network_peering.vnet2_to_vnet1
 }
+
