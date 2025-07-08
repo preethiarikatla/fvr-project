7c492f5f2a068ad15b3c07979da37af2572ceff6
pree <123841299+preethiarikatla@users.noreply.github.com>
2025-07-08 16:59:23 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -30,7 +30,7 @@ resource "azurerm_virtual_network" "vnet2" {
 }
 
 resource "azurerm_virtual_network_peering" "vnet1_to_vnet2" {
-  name                          = var.pree
+  name                          = "peer-vnet1-to-vnet2-nani"
   resource_group_name           = azurerm_resource_group.example.name
   virtual_network_name          = azurerm_virtual_network.vnet1.name
   remote_virtual_network_id     = azurerm_virtual_network.vnet2.id
@@ -40,7 +40,7 @@ resource "azurerm_virtual_network_peering" "vnet1_to_vnet2" {
   use_remote_gateways           = false
 }
 resource "azurerm_virtual_network_peering" "vnet2_to_vnet1" {
-  name                          = var.pree
+  name                          = "peer-vnet1-to-vnet2-nani"
   resource_group_name           = azurerm_resource_group.example.name
   virtual_network_name          = azurerm_virtual_network.vnet2.name
   remote_virtual_network_id     = azurerm_virtual_network.vnet1.id
@@ -49,22 +49,4 @@ resource "azurerm_virtual_network_peering" "vnet2_to_vnet1" {
   allow_gateway_transit         = false
   use_remote_gateways           = false
 }
-resource "azurerm_network_security_group" "nsg" {
-  name                = "peer-vnet1-to-vnet2-nani"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
 
-  security_rule {
-    name                       = "Allow-Internal-Peering"
-    priority                   = 100
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "*"
-    source_port_range          = "*"
-    destination_port_range     = "*"
-    source_address_prefix      = tolist(azurerm_virtual_network.vnet2.address_space)[0]
-    destination_address_prefix = tolist(azurerm_virtual_network.vnet1.address_space)[0]
-  }
-
-  depends_on = [azurerm_virtual_network_peering.vnet1_to_vnet2]
-}
