c0cbfc50cd6f6d24db8bcf187d0f8a61791cfb5a
pree <123841299+preethiarikatla@users.noreply.github.com>
2025-07-10 16:59:07 -0400

    Update moved_block.tf

--- moved_block.tf
+++ moved_block.tf
@@ -1,11 +1,11 @@
 import {
   to = azurerm_virtual_network_peering.vnet1_to_vnet2_ne
-  id = "/subscriptions/7e7e9b8c-d389-4d54-ae57-03a451e26d57/resourceGroups/rg-ignore-testt/providers/Microsoft.Network/virtualNetworks/vnet-source/virtualNetworkPeerings/peer-vnet1-to-vnet2-nani"
+  id = "/subscriptions/7e7e9b8c-d389-4d54-ae57-03a451e26d57/resourceGroups/rg-ignore-testt/providers/Microsoft.Network/virtualNetworks/vnet-source/virtualNetworkPeerings/peer-vnet1-to-vnet2-nan"
 }
 
 import {
   to = azurerm_virtual_network_peering.vnet2_to_vnet1_ne
-  id = "/subscriptions/7e7e9b8c-d389-4d54-ae57-03a451e26d57/resourceGroups/rg-ignore-testt/providers/Microsoft.Network/virtualNetworks/vnet-destination/virtualNetworkPeerings/peer-vnet1-to-vnet2-nani"
+  id = "/subscriptions/7e7e9b8c-d389-4d54-ae57-03a451e26d57/resourceGroups/rg-ignore-testt/providers/Microsoft.Network/virtualNetworks/vnet-destination/virtualNetworkPeerings/peer-vnet1-to-vnet2-nan"
 }
 
 
