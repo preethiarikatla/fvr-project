57b7fda4bda0a8a9c97e63e3c5e57cd47f146521
pree <123841299+preethiarikatla@users.noreply.github.com>
2025-07-10 12:03:08 -0400

    Update moved_block.tf

--- moved_block.tf
+++ moved_block.tf
@@ -1,11 +1,11 @@
 moved {
   from = azurerm_virtual_network_peering.vnet1_to_vnet2
-  to   = azurerm_virtual_network_peering.vnet1_to_vnet2
+  to   = azurerm_virtual_network_peering.vnet1_to_vnet2_new
 }
 
 moved {
   from = azurerm_virtual_network_peering.vnet2_to_vnet1
-  to   = azurerm_virtual_network_peering.vnet2_to_vnet1
+  to   = azurerm_virtual_network_peering.vnet2_to_vnet1_new
 }
 
 
