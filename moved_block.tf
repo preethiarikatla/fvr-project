63fdfed913ffe94695f10f18379d6c8598ad9193
pree <123841299+preethiarikatla@users.noreply.github.com>
2025-07-10 11:53:34 -0400

    Update and rename import.tf to moved_block.tf

---
+++ moved_block.tf
@@ -0,0 +1,13 @@
+moved {
+  from = azurerm_virtual_network_peering.vnet1_to_vnet2
+  to   = azurerm_virtual_network_peering.vnet1_to_vnet2
+}
+
+moved {
+  from = azurerm_virtual_network_peering.vnet2_to_vnet1
+  to   = azurerm_virtual_network_peering.vnet2_to_vnet1
+}
+
+
+
+
