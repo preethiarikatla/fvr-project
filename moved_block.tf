c70658ebfc64ed207f060b6e93ab5559f41b168a
pree <123841299+preethiarikatla@users.noreply.github.com>
2025-07-11 08:28:18 -0400

    Update moved_block.tf

--- moved_block.tf
+++ moved_block.tf
@@ -1,6 +1,6 @@
 
 import {
-  to = azurerm_virtual_network_peering.vnet1_to_vnet2_ne
+  to = azurerm_resource_group.exam
   id = "/subscriptions/7e7e9b8c-d389-4d54-ae57-03a451e26d57/resourceGroups/pree"
 }
 
