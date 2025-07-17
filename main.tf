88ba761f83dea064984230c827e77e11429bc057
pree <123841299+preethiarikatla@users.noreply.github.com>
2025-07-17 16:22:54 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -165,11 +165,6 @@ resource "azurerm_linux_virtual_machine" "vm_v2" {
     caching              = "ReadWrite"
     storage_account_type = "Standard_LRS"
   }
-  custom_data = base64encode(<<EOF
-#cloud-config
-runcmd:
-  - shutdown -h now
-EOF
-  )
+
 }
 
