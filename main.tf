ef1794e703c0a461fb0af47c0897cd6de0e32e46
pree <123841299+preethiarikatla@users.noreply.github.com>
2025-07-17 16:09:34 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -126,7 +126,7 @@ resource "azurerm_network_interface" "nic_v2" {
     name                          = "internal"
     subnet_id                     = azurerm_subnet.subnet.id
     private_ip_address_allocation = "Dynamic"
-    #public_ip_address_id          = azurerm_public_ip.pip.id
+    public_ip_address_id          = azurerm_public_ip.pip.id
   }
 }
 
@@ -136,8 +136,8 @@ resource "azurerm_linux_virtual_machine" "vm_v2" {
   location                        = azurerm_resource_group.rg.location
   resource_group_name             = azurerm_resource_group.rg.name
   size                            = "Standard_B1s"
-  #network_interface_ids           = [azurerm_network_interface.nic_v2.id]
-  network_interface_ids           = [azurerm_network_interface.nic.id]
+  network_interface_ids           = [azurerm_network_interface.nic_v2.id]
+  #network_interface_ids           = [azurerm_network_interface.nic.id]
   admin_username                  = "azureuser"
   disable_password_authentication = true
 
