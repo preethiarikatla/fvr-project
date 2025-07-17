790360baa70195a15aa3fb12ee0608144099c7e3
pree <123841299+preethiarikatla@users.noreply.github.com>
2025-07-17 15:20:36 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -135,7 +135,9 @@ resource "azurerm_linux_virtual_machine" "vm_v2" {
   location                        = azurerm_resource_group.rg.location
   resource_group_name             = azurerm_resource_group.rg.name
   size                            = "Standard_B1s"
-  network_interface_ids           = [azurerm_network_interface.nic_v2.id]
+  #network_interface_ids           = [azurerm_network_interface.nic_v2.id]
+  network_interface_ids           = [azurerm_network_interface.nic.id]
+  network_interface_ids           = [azurerm_network_interface.nic.id]
   admin_username                  = azurerm_linux_virtual_machine.vm.admin_username
   disable_password_authentication = true
 
