0166264f45559281b8d7b80a0788c34c92fe3827
pree <123841299+preethiarikatla@users.noreply.github.com>
2025-07-18 12:28:10 -0400

    Update maiin.tf

--- maiin.tf
+++ maiin.tf
@@ -12,6 +12,6 @@ provider "azurerm" {
 
 # Step 1: Create resource group
 resource "azurerm_resource_group" "rg" {
-  name     = var.resource_group_name
-  location = var.location
+  name     = "pree"
+  location = "East US"
 }
