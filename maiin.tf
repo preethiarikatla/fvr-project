0603162a396cd2fb6a0b00045396e5a8edfae59d
pree <123841299+preethiarikatla@users.noreply.github.com>
2025-07-18 15:06:38 -0400

    Update maiin.tf

--- maiin.tf
+++ maiin.tf
@@ -38,7 +38,11 @@ data "azurerm_network_interface" "vm_nic" {
   name                = "heip279_z3"
   resource_group_name = azurerm_resource_group.rg.name
 }
-
+data "azurerm_subnet" "vm_subnet" {
+  name                 = azurerm_subnet.subnet.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  resource_group_name  = azurerm_resource_group.rg.name
+}
 resource "azurerm_resource_group_template_deployment" "patch_nic_ip" {
   name                = "patch-nic"
   resource_group_name = azurerm_resource_group.rg.name
@@ -59,6 +63,9 @@ resource "azurerm_resource_group_template_deployment" "patch_nic_ip" {
               "name" = "ipconfig1"
               "properties" = {
                 "privateIPAllocationMethod" = "Dynamic"
+                "subnet" = {
+                  "id" = data.azurerm_subnet.vm_subnet.id
+                }
                 "publicIPAddress" = {
                   "id" = data.azurerm_public_ip.reserved_ip.id
                 }
@@ -70,6 +77,10 @@ resource "azurerm_resource_group_template_deployment" "patch_nic_ip" {
     ]
   })
 
-  parameters_content = jsonencode({}) # no external parameters
-  depends_on = [data.azurerm_network_interface.vm_nic, data.azurerm_public_ip.reserved_ip]
+  parameters_content = jsonencode({})
+  depends_on = [
+    data.azurerm_network_interface.vm_nic,
+    data.azurerm_public_ip.reserved_ip,
+    data.azurerm_subnet.vm_subnet
+  ]
 }
