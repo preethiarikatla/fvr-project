bf1c066d96c876bf21690a4e7c84e9e9c607157d
pree <123841299+preethiarikatla@users.noreply.github.com>
2025-07-18 15:01:47 -0400

    Update maiin.tf

--- maiin.tf
+++ maiin.tf
@@ -38,3 +38,38 @@ data "azurerm_network_interface" "vm_nic" {
   name                = "heip279_z3"
   resource_group_name = azurerm_resource_group.rg.name
 }
+
+resource "azurerm_resource_group_template_deployment" "patch_nic_ip" {
+  name                = "patch-nic"
+  resource_group_name = azurerm_resource_group.rg.name
+  deployment_mode     = "Incremental"
+
+  template_content = jsonencode({
+    "$schema" = "https://schema.management.azure.com/schemas/2019-04-01/deploymentTemplate.json#"
+    "contentVersion" = "1.0.0.0"
+    "resources" = [
+      {
+        "type" = "Microsoft.Network/networkInterfaces"
+        "apiVersion" = "2023-09-01"
+        "name" = data.azurerm_network_interface.vm_nic.name
+        "location" = azurerm_resource_group.rg.location
+        "properties" = {
+          "ipConfigurations" = [
+            {
+              "name" = "ipconfig1"
+              "properties" = {
+                "privateIPAllocationMethod" = "Dynamic"
+                "publicIPAddress" = {
+                  "id" = data.azurerm_public_ip.reserved_ip.id
+                }
+              }
+            }
+          ]
+        }
+      }
+    ]
+  })
+
+  parameters_content = jsonencode({}) # no external parameters
+  depends_on = [data.azurerm_network_interface.vm_nic, data.azurerm_public_ip.reserved_ip]
+}
