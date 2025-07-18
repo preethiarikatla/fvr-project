95f5e538190a42220d2cc060e5a5da2fbe5ab2bb
pree <123841299+preethiarikatla@users.noreply.github.com>
2025-07-18 13:25:37 -0400

    Update maiin.tf

--- maiin.tf
+++ maiin.tf
@@ -30,4 +30,37 @@ resource "azurerm_subnet" "subnet" {
   address_prefixes     = ["10.0.1.0/24"]
 }
 
+data "azurerm_network_interface" "existing_nic" {
+  name                = "he119_z3"
+  resource_group_name = azurerm_resource_group.rg.name
+}
 
+# Step 2: Fetch the manually created static public IP
+data "azurerm_public_ip" "reserved_ip" {
+  name                = "test1"
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+# Step 3: Update the NIC to use static public IP
+resource "azurerm_network_interface" "nic_update" {
+  name                = data.azurerm_network_interface.existing_nic.name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+
+  ip_configuration {
+    name                          = data.azurerm_network_interface.existing_nic.ip_configuration[0].name
+    subnet_id                     = data.azurerm_network_interface.existing_nic.ip_configuration[0].subnet_id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = data.azurerm_public_ip.reserved_ip.id
+  }
+
+  lifecycle {
+    ignore_changes = [
+      ip_configuration[0].private_ip_address_allocation
+    ]
+  }
+
+  depends_on = [
+    data.azurerm_network_interface.existing_nic
+  ]
+}
