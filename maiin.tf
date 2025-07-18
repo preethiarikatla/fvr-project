6914b8a2459be30b4c08059918c22a5a3ee54eba
pree <123841299+preethiarikatla@users.noreply.github.com>
2025-07-18 12:25:22 -0400

    Update maiin.tf

--- maiin.tf
+++ maiin.tf
@@ -1,3 +1,15 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>4.19.0"
+    }
+  }
+}
+provider "azurerm" {
+  features {}
+}
+
 # Step 1: Create resource group
 resource "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
