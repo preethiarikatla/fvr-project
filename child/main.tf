d9b221e2d5a6e2fefc11e1a77d1966519c386f70
pree <123841299+preethiarikatla@users.noreply.github.com>
2025-07-21 17:47:33 -0400

    Update main.tf

--- child/main.tf
+++ child/main.tf
@@ -12,76 +12,60 @@ provider "azurerm" {
 
 # Step 1: Create resource group
 resource "azurerm_resource_group" "rg" {
-  name     = "pree"
-  location = "East US"
+  name     = var.resource_group_name
+  location = var.location
 }
 
 resource "azurerm_virtual_network" "vnet" {
   name                = "test-vnet"
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  location            = var.location
+  resource_group_name = var.resource_group_name
 }
 
 resource "azurerm_subnet" "subnet" {
   name                 = "test-subnet"
-  resource_group_name  = azurerm_resource_group.rg.name
+  resource_group_name  = var.resource_group_name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
 }
-
-data "azurerm_public_ip" "reserved_ip" {
-  name                = "gigy"
-  resource_group_name = azurerm_resource_group.rg.name
+ resource "azurerm_network_interface" "nic" {
+  name                = var.nic_name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+ 
+   ip_configuration {
+     name                          = "internal"
+     subnet_id                     = azurerm_subnet.subnet.id
+     private_ip_address_allocation = "Dynamic"
+   }
 }
-data "azurerm_network_interface" "vm_nic" {
-  name                = "heip279_z3"
-  resource_group_name = azurerm_resource_group.rg.name
+resource "azurerm_linux_virtual_machine" "vm_v2" {
+   name                            = "copilot-test-vm-v2"
+   location                        = azurerm_resource_group.rg.location
+   resource_group_name             = azurerm_resource_group.rg.name
+   size                            = "Standard_B1s"
+   #network_interface_ids           = [azurerm_network_interface.nic_v2.id]
+   network_interface_ids           = [azurerm_network_interface.nic.id]
+   admin_username                  = "azureuser"
+   disable_password_authentication = true
+ 
+   admin_ssh_key {
+     username   = "azureuser"
+     public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDRJaB9f+o1bWUQFfigorqJVfcLNKX2Ox29MtvqyPgMz4D/WuSpa09nIbgp195vuqLbHiGG0gV2WNQab1MOLbI8xSm9wLNyX0Srm4+jwWXylHpjflm3L1QnceQANnt2LVqr7h2mSMubytDxKhImOnSXejgylyVp+nFV0624lHuyJXDNHZl+RXC0giEE1Iujz3Mu2lyZ1DkWAYzAbvvZfu8jOVuSk8hdpjZn6k0jvMkBGbCNxyg18SM/TSgx5X5Mwszjbx2dU1tNpXfW87XcvRn9zVE7Asw196YoZHx2yRadEf1KCv+vJxW/6Pwu1V7Uqg4k2t58rJ46217l39ZlKUJ9 preethi@SandboxHost-638883515602013682"
+   }
+ 
+   source_image_reference {
+     publisher = "Canonical"
+     offer     = "UbuntuServer"
+     sku       = "18.04-LTS"
+     version   = "latest"
+   }
+ 
+   os_disk {
+     name                 = "copilot-osdisk-v2"
+     caching              = "ReadWrite"
+     storage_account_type = "Standard_LRS"
 }
-data "azurerm_subnet" "vm_subnet" {
-  name                 = azurerm_subnet.subnet.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  resource_group_name  = azurerm_resource_group.rg.name
-}
-resource "azurerm_resource_group_template_deployment" "patch_nic_ip" {
-  name                = "patch-nic"
-  resource_group_name = azurerm_resource_group.rg.name
-  deployment_mode     = "Incremental"
-
-  template_content = jsonencode({
-    "$schema" = "https://schema.management.azure.com/schemas/2019-04-01/deploymentTemplate.json#"
-    "contentVersion" = "1.0.0.0"
-    "resources" = [
-      {
-        "type" = "Microsoft.Network/networkInterfaces"
-        "apiVersion" = "2023-09-01"
-        "name" = data.azurerm_network_interface.vm_nic.name
-        "location" = azurerm_resource_group.rg.location
-        "properties" = {
-          "ipConfigurations" = [
-            {
-              "name" = "ipconfig1"
-              "properties" = {
-                "privateIPAllocationMethod" = "Dynamic"
-                "subnet" = {
-                  "id" = data.azurerm_subnet.vm_subnet.id
-                }
-                "publicIPAddress" = {
-                  "id" = data.azurerm_public_ip.reserved_ip.id
-                }
-              }
-            }
-          ]
-        }
-      }
-    ]
-  })
-
-  parameters_content = jsonencode({})
-  depends_on = [
-    data.azurerm_network_interface.vm_nic,
-    data.azurerm_public_ip.reserved_ip,
-    data.azurerm_subnet.vm_subnet
-  ]
 }
 
