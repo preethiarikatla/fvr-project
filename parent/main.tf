24781b32ec8bd8f3ac3fed7734fd0c651b2c0a92
pree <123841299+preethiarikatla@users.noreply.github.com>
2025-07-21 18:55:13 -0400

    Update main.tf

--- parent/main.tf
+++ parent/main.tf
@@ -10,45 +10,11 @@ provider "azurerm" {
   features {}
 }
 
-data "terraform_remote_state" "child" {
-  backend = "remote"
 
-  config = {
-    organization = "tesy"        
-    workspaces = {
-      name = "pre"                    
-    }
-  }
-}
-
-
-# Set local for NIC metadata
-locals {
-  nic_metadata = data.terraform_remote_state.child.outputs.nic_metadata
-}
-
-# Fetch NICs with name + RG
-data "azurerm_network_interface" "fetched_nics" {
-  for_each = local.nic_metadata
-
-  name                = each.value.name
-  resource_group_name = each.value.resource_group_name
-}
 # Step 4: Pass NIC info to child patch module
 module "ubuntu_vm" {
   source              = "../child"
   resource_group_name = "pree"
   location            = "East US"
 
-  egress_nic_names = {
-    for k, nic in data.azurerm_network_interface.fetched_nics : k => nic.name
-  }
-
-  egress_nic_locations = {
-    for k, nic in data.azurerm_network_interface.fetched_nics : k => nic.location
-  }
-
-  egress_ipconfig_names = {
-    for k, nic in data.azurerm_network_interface.fetched_nics : k => nic.ip_configuration[0].name
-  }
 }
