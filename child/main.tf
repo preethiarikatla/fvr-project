31c405cdeb5b519e1d8a13c63df950d02caa80a2
pree <123841299+preethiarikatla@users.noreply.github.com>
2025-07-21 19:19:58 -0400

    Update main.tf

--- child/main.tf
+++ child/main.tf
@@ -29,3 +29,17 @@ resource "azurerm_subnet" "subnet" {
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
 }
+
+resource "azurerm_network_interface" "nic" {
+  for_each = toset(["av-nic3", "av-nic4"])
+
+  name                = each.key
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
