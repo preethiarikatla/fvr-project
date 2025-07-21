070fd89cf544b8db021be09f084392d09d6c6e47
pree <123841299+preethiarikatla@users.noreply.github.com>
2025-07-21 18:47:06 -0400

    Update outputs.tf

--- child/outputs.tf
+++ child/outputs.tf
@@ -2,8 +2,8 @@ output "nic_metadata" {
   value = {
     for k, nic in azurerm_network_interface.nic :
     k => {
-      name                = nic.name
-      resource_group_name = nic.resource_group_name
+      name                = azurerm_network_interface.nic.name
+      resource_group_name = azurerm_network_interface.nic.resource_group_name
     }
   }
 }
