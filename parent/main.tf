108665957c3ea96b98eb9a1db3965d07a17c28f6
pree <123841299+preethiarikatla@users.noreply.github.com>
2025-07-21 18:20:02 -0400

    Update main.tf

--- parent/main.tf
+++ parent/main.tf
@@ -30,8 +30,30 @@ data "terraform_remote_state" "transitgw" {
   }
 }
 
-# Fetch NICs
+# Example NIC structure to pass (if multiple NICs are involved)
+locals {
+  nic_ids = data.terraform_remote_state.child.outputs.nic_ids
+}
+
 data "azurerm_network_interface" "fetched_nics" {
-  for_each = data.terraform_remote_state.transitgw.outputs.nic_ids
+  for_each = local.nic_ids
   id       = each.value
+}
+
+module "patch_nics" {
+  source              = "../modules/child"
+  resource_group_name = "pree"
+  location            = "East US"
+
+  egress_nic_names = {
+    for k, nic in data.azurerm_network_interface.fetched_nics : k => nic.name
+  }
+
+  egress_nic_locations = {
+    for k, nic in data.azurerm_network_interface.fetched_nics : k => nic.location
+  }
+
+  egress_ipconfig_names = {
+    for k, nic in data.azurerm_network_interface.fetched_nics : k => nic.ip_configuration[0].name
+  }
 }
