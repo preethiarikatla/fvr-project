14d1408db1a2025f3fe29b2cd3d4db185b4b0789
pree <123841299+preethiarikatla@users.noreply.github.com>
2025-07-21 17:26:16 -0400

    Update main.tf

--- FVR PROJECT/css/main.tf
+++ FVR PROJECT/css/main.tf
@@ -1,1 +1,73 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>4.19.0"
+    }
+  }
+}
+provider "azurerm" {
+  features {}
+}
 
+resource "azurerm_resource_group" "rg" {
+  name     = "patch-nic-rg"
+  location = "East US"
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "patch-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "patch-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_network_interface" "vm_nic" {
+  name                = "heip279_z3"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "ipconfig1"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+# Optional: Deploy a VM if you want
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                = "vm-noip"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = "Standard_B1s"
+  admin_username      = "azureuser"
+  network_interface_ids = [
+    azurerm_network_interface.vm_nic.id
+  ]
+  admin_password = "Password1234!"
+  disable_password_authentication = false
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "20_04-lts"
+    version   = "latest"
+  }
+}
+
+# Output the NIC ID
+output "egress_nic_ids" {
+  value = {
+    fw1 = azurerm_network_interface.vm_nic.id
+  }
+}
