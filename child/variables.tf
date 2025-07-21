56fe9e8831a79047c2b45fa00cc5e28ea377cf29
pree <123841299+preethiarikatla@users.noreply.github.com>
2025-07-21 18:54:37 -0400

    Update variables.tf

--- child/variables.tf
+++ child/variables.tf
@@ -1,12 +1,4 @@
 variable "resource_group_name" {}
 variable "location" {}
 
-variable "egress_nic_names" {
-  type = map(string)
-}
-variable "egress_nic_locations" {
-  type = map(string)
-}
-variable "egress_ipconfig_names" {
-  type = map(string)
-}
+
